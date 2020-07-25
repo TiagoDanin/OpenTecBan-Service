7e0cf0a20fdc4d31a46f41ba975896d255623b32
Tiago Danin <TiagoDanin@outlook.com>
2020-07-25 13:21:00 -0300

    feat: create endpoint to get bicos

---
+++ database.js
@@ -0,0 +1,28 @@
+const jsonfile = require('jsonfile')
+const { v4: uuidv4 } = require('uuid');
+
+const file = 'data.json'
+const bicos = jsonfile.readFileSync(file)
+
+const save = () => {
+	jsonfile.writeFileSync(file, bicos, {
+		replacer: true,
+		spaces: '\t'
+	})
+}
+
+const addBico = (name, photo, description) => {
+	const user = {
+		id: uuidv4(),
+		name, photo, description
+	}
+
+	bicos.list.push(user)
+	save()
+	return user
+}
+
+module.exports = {
+	bicos,
+	addBico
+}
--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const api = require('./api')
+const database = require('./database')
 
 const port = process.env.PORT
 
 let token = ''
+
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
 app.get('/', (request, responseExpress) => responseExpress.send('Hello World!'))
 
@@ -42,6 +49,23 @@ app.get('/startAuthentication/:id', (request, responseExpress) => {
 				.catch(error)
 		})
 		.catch(error)
+})
+
+app.get('/bicos/all', (request, responseExpress) => {
+	responseExpress.json({isOk: true, list: database.bicos.list})
+})
+
+app.get('/bicos/:id', (request, responseExpress) => {
+	const id = request.params.id
+	const list = database.bicos.list.filter(bico => bico.id === id)
+
+	responseExpress.json({isOk: true, list})
+})
+
+app.post('/bicos/add', (request, responseExpress) => {
+	const user = database.addBico(request.body.name, request.body.photo, request.body.description)
+
+	responseExpress.json({isOk: true, user, list: database.bicos.list})
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
